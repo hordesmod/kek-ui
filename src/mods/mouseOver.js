47ba54a9b19b594d29383777f9a5fa4f5268b999
hordesmod <rgh36dn@gmail.com>
2026-03-10 20:41:21 +0300

    kek-ui

--- src/mods/mouseOver.js
+++ src/mods/mouseOver.js
@@ -10,10 +10,40 @@ const mouseOver = {
         enabled: false
     },
     hovered: null,
+    start() {
+        eventManager.on("ui.sysbtnbar", this.addBtn, this)
+    },
+    stop() {
+        eventManager.off("ui.sysbtnbar", this.addBtn, this)
+        this.btn = this.btn.remove()
+    },
     toggle() {
         this.state.enabled = !this.state.enabled
     },
-    start() {
+    btn: 0,
+    change: 0,
+    addBtn(sysbtnbar) {
+        this.btn = element("div")
+            .css("btn border black textgrey")
+            .style({
+                paddingLeft: "3px",
+                paddingRight: "3px",
+                margin: "2px"
+            })
+            .text("Mo")
+            .on("click", this.toggleBtn.bind(this))
+        this.btn.element.tooltip = "Mouse over mode"
+
+        addSysbtn(sysbtnbar.element, this.btn.element)
+        // partyBtnbar.element.appendChild(this.btn.element)
+    },
+    toggleBtn() {
+        this.btn.toggle("textred").toggle("textgrey")
+        this.change ^= 1
+        this.toggleMode()
+    },
+
+    toggleMode2(){
         document.addEventListener("mouseover", (e) => {
             if (!this.state.enabled) return
             const t = e.target.closest(".bghealth, .left, .right")
@@ -25,9 +55,6 @@ const mouseOver = {
             this.hovered.click()
         })
         eventManager.on("click.mouseOver", this.toggle, this)
-    },
-    stop() {
-        eventManager.off("click.mouseOver", this.toggle, this)
     },
 }
 
